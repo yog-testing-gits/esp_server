c41c3058e8e8434c960aebb2f42850b6a84330fb
yog-testing-gits <109839967+yog-testing-gits@users.noreply.github.com>
2025-03-31 04:47:37 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("ESP32 Server Running!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
